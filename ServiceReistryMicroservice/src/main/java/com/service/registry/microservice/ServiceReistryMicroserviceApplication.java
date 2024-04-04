49b57c9ae7ac4f4f6dbe48429e9a01c740e13047
Ramkalame <ramkalame14082001@gmail.com>
2024-04-05 00:34:55 +0530

    project is intialised

---
+++ ServiceReistryMicroservice/src/main/java/com/service/registry/microservice/ServiceReistryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.service.registry.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceReistryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceReistryMicroserviceApplication.class, args);
+	}
+
+}
